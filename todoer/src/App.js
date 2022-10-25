4414f2af83ffdb2613aea45ea3aa6c2561e61446
mata-m <m.mata3698@gmail.com>
2022-10-25 00:39:08 -0700

    Added Todo List Form Component for Input and Item Filtering.

--- todoer/src/App.js
+++ todoer/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+// Importing Components
+import Form from "./components/Form";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello react </h1>
+      <header>Todo List</header>
+      <Form />
     </div>
   );
 }
