abf4ee87427f4571cdf55ebd3538cd72c3e06268
mata-m <m.mata3698@gmail.com>
2022-10-25 00:52:53 -0700

    Added initial Todo List Component.

--- todoer/src/App.js
+++ todoer/src/App.js
@@ -1,13 +1,15 @@
+import React, { useState } from 'react';
 import './App.css';
 // Importing Components
 import Form from "./components/Form";
-
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <div className="App">
       <header>Todo List</header>
       <Form />
+      <TodoList />
     </div>
   );
 }
